4da8d537baa75a728d8439c21a3cf3e46e7e741f
alberto56 <albert@mediatribe.net>
2016-11-18 12:14:58 -0500

    #7 https://github.com/docker/compose/issues/3379

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,15 +5,17 @@
 set -e
 
 # Create the environment.
-./scripts/run.sh
+#./scripts/run.sh
 
 # Run the migration as described in the README.md
-docker-compose exec drupal8 /bin/bash -c 'drush en -y my_migration'
-docker-compose exec drupal8 /bin/bash -c 'drush cc drush'
-docker-compose exec drupal8 /bin/bash -c 'drush migrate-status'
-docker-compose exec drupal8 /bin/bash -c 'drush migrate-import --all'
+# We will use docker exec instead of docker-compose exec because of
+# https://github.com/docker/compose/issues/3379
+docker exec "$(docker-compose ps -q drupal8)" /bin/bash -c 'drush en -y my_migration'
+docker exec "$(docker-compose ps -q drupal8)" /bin/bash -c 'drush cc drush'
+docker exec "$(docker-compose ps -q drupal8)" /bin/bash -c 'drush migrate-status'
+docker exec "$(docker-compose ps -q drupal8)" /bin/bash -c 'drush migrate-import --all'
 
-docker-compose exec drupal8 /bin/bash -c 'drush eval my_migration_test()'
+docker exec "$(docker-compose ps -q drupal8)" /bin/bash -c 'drush eval "my_migration_test()"'
 
 # Make sure we can restore Drupal 8 to its pristine state.
 ./scripts/restore-newly-installed.sh
