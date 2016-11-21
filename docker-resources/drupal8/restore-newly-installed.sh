95e57be282a2727e45b4364670ad2ec6d6f89474
alberto56 <albert@mediatribe.net>
2016-11-21 12:20:26 -0500

    Ignore error if no files on chown

--- docker-resources/drupal8/restore-newly-installed.sh
+++ docker-resources/drupal8/restore-newly-installed.sh
@@ -7,7 +7,8 @@ set -e
 rm -rf /var/www/html/sites/default/files/*
 cp -r /newly-installed-files/* /var/www/html/sites/default/files/ \
   2>/dev/null || :
-chown -R www-data:www-data /var/www/html/sites/default/files/*
+chown -R www-data:www-data /var/www/html/sites/default/files/* \
+  2>/dev/null || :
 echo "drop database drupal" | mysql --user=drupal --password=drupal --host=drupal8database
 echo "create database drupal" | mysql --user=drupal --password=drupal --host=drupal8database
 drush sqlc < /newly-installed-database.sql
